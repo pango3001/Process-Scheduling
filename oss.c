7a59b82d052b8799030115e63e9be81d745e2097
pango3001 <pango3001@gmail.com>
2020-10-25 14:01:46 -0500

    removed somethings (cleaned up)

--- oss.c
+++ oss.c
@@ -10,8 +10,94 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "oss.h"
-#include "user_proc.h"
+
+#define TRUE 1
+#define FALSE 0
+
+
+//msg struct for msgqueue
+typedef struct {
+    long mtype;
+    int mvalue;
+} mymsg_t;
+
+//simulated time value
+//used for the simulated clock
+typedef struct {
+    unsigned int s;
+    unsigned int ns;
+} simtime_t;
+
+
+//pseudo-process control block
+//used for PCB Table
+typedef struct {
+    //simulated process id, range is [0,18]
+    int pid;
+    //Process priority
+    int priority;
+    //if the process is ready to run
+    int isReady;
+    //Arrivial time
+    simtime_t arrivalTime;
+    //CPU time used
+    simtime_t cpuTime;
+    //Time in the system
+    simtime_t sysTime;
+    //Time used in the last burst
+    simtime_t burstTime;
+    //Total sleep time. time waiting for an event
+    simtime_t waitTime;
+
+
+} pcb_t;
+
+typedef struct {
+    unsigned int head;
+    unsigned int tail;
+    unsigned int size;
+    unsigned int items;
+    int* data;
+} queue_t;
+
+queue_t* create_queue(int size) {
+    queue_t* queue = (queue_t*)malloc(sizeof(queue_t));
+    queue->head = 0;
+    queue->tail = 0;
+    queue->size = size;
+    queue->data = (int*)malloc(size * sizeof(int));
+    queue->items = 0;
+    int i;//loop iterator
+    for (i = 0; i < size; i++)
+        queue->data[i] = -1;//set all elements to -1. no pids will be -1
+    return queue;
+}
+
+void enqueue(queue_t* queue, int pid) {
+    queue->data[queue->tail] = pid;//add pid to queue
+    queue->tail = (queue->tail + 1) % queue->size;
+    queue->items += 1;
+    return;
+}
+
+int dequeue(queue_t* queue) {
+    int pid = queue->data[queue->head];
+    queue->head = (queue->head + 1) % queue->size;
+    queue->items -= 1;
+    return pid;
+}
+
+FILE* logFile;//log file
+const key_t PCB_TABLE_KEY = 110594;//key for shared PCB Table
+const key_t CLOCK_KEY = 110197;//key for shared simulated clock 
+const key_t MSG_KEY = 052455;//key for message queue
+int pcbTableId;//shmid for PCB Table
+int clockId;//shmid for simulated clock
+int msqid;//id for message queue
+
+
+
+
 
 // Prototypes
 void no_args_msg();
@@ -22,17 +108,78 @@ void cleanup();
 void remove_shm();
 
 pcb_t* create_table(int);
-simtime_t* create_sim_clock();
+
 void create_msqueue();
 
 int get_sim_pid(int*, int);
-simtime_t get_next_process_time(simtime_t, simtime_t);
+
 int rand_priority(int);
-int should_spawn(int, simtime_t, simtime_t, int, int);
+
 int check_blocked(int*, pcb_t*, int);
 
 void oss(int);
 int dispatch(int, int, int, simtime_t, int, int*);
+
+
+simtime_t* create_sim_clock();
+simtime_t get_next_process_time(simtime_t, simtime_t);
+int should_spawn(int, simtime_t, simtime_t, int, int);
+
+
+
+
+
+
+//increment given simulated time by given increment
+void increment_sim_time(simtime_t* simTime, int increment) {
+    simTime->ns += increment;
+    if (simTime->ns >= 1000000000) {
+        simTime->ns -= 1000000000;
+        simTime->s += 1;
+    }
+}
+// returns a - b
+simtime_t subtract_sim_times(simtime_t a, simtime_t b) {
+    simtime_t diff = { .s = a.s - b.s,
+                      .ns = a.ns - b.ns };
+    if (diff.ns < 0) {
+        diff.ns += 1000000000;
+        diff.s -= 1;
+    }
+    return diff;
+}
+//returns a + b
+simtime_t add_sim_times(simtime_t a, simtime_t b) {
+    simtime_t sum = { .s = a.s + b.s,
+                      .ns = a.ns + b.ns };
+    if (sum.ns >= 1000000000) {
+        sum.ns -= 1000000000;
+        sum.s += 1;
+    }
+    return sum;
+}
+
+//returns simtime / divisor
+simtime_t divide_sim_time(simtime_t simTime, int divisor) {
+    simtime_t quotient = { .s = simTime.s / divisor, .ns = simTime.ns / divisor };
+    return quotient;
+}
+
+pcb_t create_pcb(int priority, int pid, simtime_t currentTime) {
+    pcb_t pcb = { .pid = pid,
+                  .priority = priority,
+                  .isReady = TRUE,
+                  .arrivalTime = {.s = currentTime.s, .ns = currentTime.ns},
+                  .cpuTime = {.s = 0, .ns = 0},
+                  .sysTime = {.s = 0, .ns = 0},
+                  .burstTime = {.s = 0, .ns = 0},
+                  .waitTime = {.s = 0, .ns = 0} };
+    return pcb;
+}
+
+
+
+
 
 int main(int argc, char* argv[]) {
     int opt;                       // getopt option
