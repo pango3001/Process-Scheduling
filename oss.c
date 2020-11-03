8edcdc7e863ffbf7c8ecaf540f3c6cdef2142961
pango3001 <pango3001@gmail.com>
2020-11-02 19:32:20 -0600

    added ftok for keys

--- oss.c
+++ oss.c
@@ -57,9 +57,16 @@ typedef struct {
 const double maxTimeBetweenNewProcsNS;
 const double maxTimeBetweenNewProcsSecs;
 FILE* logFile;//log file
+/*
 const key_t PCB_TABLE_KEY = 110667;//key for shared PCB Table
 const key_t CLOCK_KEY = 110626;//key for shared simulated clock 
 const key_t MSG_KEY = 052644;//key for message queue
+*/
+
+const key_t PCB_TABLE_KEY = ftok("./oss", 'a');
+const key_t CLOCK_KEY = ftok("./oss", 'b');
+const key_t MSG_KEY = ftok("./oss", 'c');
+
 int pcbTableId;//shmid for PCB Table
 int clockId;//shmid for simulated clock
 int messageID;//id for message queue
